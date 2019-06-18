97ff4cab1dcd770be8f3c73755cad439dbe167d8
William Diffey <william.diffey@protonmail.com>
2019-06-18 12:21:17 +0900

    1. Added AirVisual API data to be displayed for the LGN/LAT as this is always useful in Korea. 2. Added aira labels for each section 3. Added a reload button to that users can reload the page and find a new address 4. Updated README with information about the apps use and name.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
 const geocodeURL = 'https://maps.googleapis.com/maps/api/geocode/json?';
 const googleKey = 'AIzaSyB58D6ZX3OPAiYJrToJCxE7g5CybwcASAA';
+const aqiURL = 'https://api.airvisual.com/v2/nearest_city?';
+const aqiKey = 'WxnLijJNB9ho8LCvz';
 
 
-
+function loadPage() {
+  $('.reloadPage').hide;
+  $('#js-error-message').hide;
+  $('js-display-results').hide;
+  autocomplete();
+}
 
 
 function autocomplete() {
-  console.log(`autocomplete is running`);
   var input = document.getElementById('AC');
   var options = {
   // types: ['(geocode)'],
@@ -15,96 +21,130 @@ function autocomplete() {
   };
   var autocomplete = new google.maps.places.Autocomplete(input, options);
   $('form').submit(event => {
-    event.preventDefault();
-    const searchAddress = $('#AC').val();
-    console.log(searchAddress);
-    formatAddress(searchAddress);
-
-  })}
-
-    function formatAddress(searchAddress) {
-    let cleanAddress = searchAddress.replace(/\s+/g, '+');
-    console.log(cleanAddress);
-    getGeo(cleanAddress);
+  event.preventDefault();
+  const searchAddress = $('#AC').val().trim();
+  console.log(searchAddress);
+  formatAddress(searchAddress);
+  })
 }
 
-   
 
-    // var place = autocomplete.getPlace();
-    // console.log(place);
-    // var lat = place.geometry.location.lat;
-    // console.log(lat);
-    // var lgn = place.geometry.location.lgn;
-    // console.log(lgn);
-  
-    
-  // places()
-
-// function watchForm() {
-//     $('form').submit(event => {
-//       event.preventDefault();
-//       const searchaddress = $('#address').val().trim();
-//       console.log(searchaddress);
-//       getGeo(searchaddress);
-//     });
+function formatAddress(searchAddress) {
+  let cleanAddress = searchAddress.replace(/\s+/g, '+');
+  getGeo(cleanAddress);
+}
 
 
 function getGeo(address) {
-  console.log(`getGeo is working`);
   const params = {
     address: address,
     key: googleKey,
     }
-const queryString = formatQueryParams(params)
-const URL = geocodeURL + queryString; 
-console.log(URL);
-
-fetch(URL)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      })
-      .then(responseJson => setLngLat(responseJson))
-      .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-      });
+  const queryString = formatQueryParams(params);
+  const URL = geocodeURL + queryString; 
+  
+  fetch(URL)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => setLngLat(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`We had a problem getting address data: ${err.message}`);
+    });
 }
-
-function setLngLat(responseJson) {
-  console.log(responseJson);
-  console.log(`set lnglats running`);
-  const LAT = responseJson.results[0].geometry.location.lat;
-  const LNG = responseJson.results[0].geometry.location.lng;
-  console.log(`lat is ${LAT}`);
-  console.log(`long is ${LNG}`);
-  createLinks(LAT,LNG);
-
-    // 37.5663797, 126.9777154 Seoul
-    // http://maps.google.com/maps/place/37.5667397,126.9777154 (no side panel)
-  // http://maps.google.com/maps?q=37.5663797,126.9777154 (side panel)
-  // http://map.daum.net/link/map/37.5663797,126.9777154 (side panel)
-  // http://map.naver.com/?elat=37.5663797&elng=126.9777154 (side panel)
-}
-
-function createLinks(LAT, LNG) {
-   $('js-display-results').empty();
-   $('#js-display-results').append(`
-  <a href="http://maps.google.com/maps?q=${LAT},${LNG}" target="_blank">Open in Google Maps</a>
-  <a href="http://map.naver.com/?elat=${LAT}&elng=${LNG}" target="_blank" >Open in Naver Maps</a>
-  <a href="http://map.daum.net/link/map/${LAT},${LNG}" target="_blank">Open in Kakao Maps</a>`)
-}
-
-
 
 
 function formatQueryParams(params) {
   console.log(`format query params is working`)
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('+CA&');
 }
 
-$(autocomplete);
+function getAQI(LAT, LGN) {
+  const aqiParams = {
+    lat: LAT,
+    lon: LGN,
+    key: aqiKey,
+  }
+  const aqiQueryString = aqiFormatQueryParams(aqiParams);
+  const URL = aqiURL + aqiQueryString; 
+  console.log(URL);
+
+      fetch(URL)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJson => setAQI(responseJson))
+        .catch(err => {
+          $('#js-aqi-error-message').text(`We had a problem getting AQI data: ${err.message}`);
+        });
+}
+
+
+function aqiFormatQueryParams(params) {
+  console.log(`format query params is working`)
+  const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+
+function setLngLat(responseJson) {
+  const LAT = responseJson.results[0].geometry.location.lat;
+  const LNG = responseJson.results[0].geometry.location.lng;
+  displayLinks(LAT,LNG);
+  getAQI(LAT,LNG);
+}
+
+
+function setAQI(responseJson) {
+  const aqiResult = responseJson.data.current.pollution.aqius;
+  console.log(aqiResult);
+  displayAQI(aqiResult);
+  
+  
+}
+
+
+function displayLinks(LAT, LNG) {
+   $('js-display-results').empty();
+   $('form').hide();
+   $('#reloadPage').show();
+   $('#js-display-results').append(`
+  <section aria-label="Links to map services">
+    <ul>
+      <li><a href="http://maps.google.com/maps?q=${LAT},${LNG}" target="_blank">Open in Google Maps</a></li>
+      <li><a href="http://map.naver.com/?elat=${LAT}&elng=${LNG}" target="_blank" >Open in Naver Maps</a></li>
+      <li><a href="http://map.daum.net/link/map/${LAT},${LNG}" target="_blank">Open in Kakao Maps</a></li>
+    </ul>
+  </section>
+  `);
+  
+  reloadPage();
+}
+
+
+function displayAQI(aqi) {
+  $('js-aqi-display-results').empty();
+  $('#js-display-results').append(`
+  <section aria-label="AQI data">
+  <p>AQI at this location is currently ${aqi}</p>
+  </section>`);
+}
+
+
+function reloadPage() {
+  $('#reloadPage').append(`<input type="button" class="reloadPage" value="Find another location"></input>`)
+  $('.reloadPage').click(function (event) {
+  location.reload();
+  });
+}
+
+
+$(loadPage);
 
